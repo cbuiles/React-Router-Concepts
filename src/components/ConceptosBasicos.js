7cb498799d8f617ed632af15ce6ec2c57b995569
cbuiles <cgomezb@unal.edu.co>
2021-10-09 14:39:47 -0500

    Clase 56. React Router: Componentes Links y NavLink

--- src/components/ConceptosBasicos.js
+++ src/components/ConceptosBasicos.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Acerca from "../pages/Acerca";
 import Contacto from "../pages/Contacto";
-import Error404 from "../pages/Error";
+import Error404 from "../pages/Error404";
 import Home from "../pages/Home";
+import MenuConceptos from "./MenuConceptos";
 
 // const ConceptosBasicos = () => {
 //   return (
@@ -50,6 +51,7 @@ const ConceptosBasicos = () => {
     <div>
       <h2>Conceptos Basicos</h2>
       <Router>
+        <MenuConceptos />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/acerca" component={Acerca} />
