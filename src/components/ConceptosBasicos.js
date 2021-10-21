b02bee529d67d6e6b336d46bd10ed82e7fdb9745
cbuiles <cgomezb@unal.edu.co>
2021-10-21 06:59:51 -0500

    Clase 63. React Router: HashRuoter

--- src/components/ConceptosBasicos.js
+++ src/components/ConceptosBasicos.js
@@ -3,6 +3,8 @@ import {
   Route,
   Switch,
   Redirect,
+  HashRouter,
+  Link,
 } from "react-router-dom";
 import Acerca from "../pages/Acerca";
 import Contacto from "../pages/Contacto";
@@ -60,6 +62,31 @@ import PrivateRoute from "./PrivateRoute";
 const ConceptosBasicos = () => {
   return (
     <div>
+      <h2>Hash Router</h2>
+      <HashRouter>
+        <MenuConceptos />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/acerca" component={Acerca} />
+          <Route exact path="/contacto" component={Contacto} />
+          <Route exact path="/usuario/:username" component={Usuario} />
+          <Route exact path="/productos" component={Productos} />
+          <Route exact path="/about">
+            <Redirect to="/acerca" />
+          </Route>
+          <Route exact path="/contact">
+            <Redirect to="/contacto" />
+          </Route>
+          {/* Cuadno se trabaja con una ruta que genera subrrutas debe omitirse el exact ya que se van a tomar variables dinamicamente */}
+          <Route path="/react" component={ReactTopics} />
+          <Route exact path="/login" component={Login} />
+          {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <Route path="*" component={Error404} />
+        </Switch>
+      </HashRouter>
+      <hr />
       <h2>Conceptos Basicos</h2>
       <Router>
         <MenuConceptos />
